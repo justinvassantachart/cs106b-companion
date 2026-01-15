8d948c4d7f0e44f53909450359f99940862f9a3d
Justin Vassantachart <justin@simplecore.org>
2026-01-15 11:29:18 -0800

    feat: modifed heap visualization example

--- src/app/companion-files.ts
+++ src/app/companion-files.ts
@@ -259,15 +259,14 @@ struct Node {
     Node* next;
 };
 
-void helper() {
+int notSoHelpfulHelper() {
     int* p = new int(42);
     // p goes out of scope, but heap object should persist in the visualizer as a memory leak
+    return 1;
 }
 
-int main() {
-    cout << "--- Heap Test ---" << endl;
-    
-    helper();
+int main() {    
+    notSoHelpfulHelper();
     
     Node* list = new Node;
     list->val = 1;
@@ -275,10 +274,11 @@ int main() {
     list->next->val = 2;
     list->next->next = nullptr;
     
-    cout << "Check Heap Visualization now." << endl;
+    cout << "Check Heap Visualization" << endl;
     
     return 0;
 }
+
 `
     },
     {
